2d4ba51fd08ff6db7e04dbe74f220fe8f506ce74
AjaSharma93 <aja.sharma1101@gmail.com>
2016-11-05 18:09:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
 var crypto=require('crypto');
+var bodyParser=require('body-parser');
 
 var config={
     user:'ajasharma93',
@@ -13,6 +14,7 @@ var config={
 }
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 var pool = new Pool(config); //declaring a connection pool for database queries;
 
@@ -25,6 +27,27 @@ function hash(input, salt)
 app.get('/hash/:input', function(req, res){
    var hashedString=hash(req.params.input, 'this-is-some-random-string');
    res.send(hashedString);
+});
+
+app.post('/create-user', function(req, res)
+{
+    var username=req.body.username;
+    var password=req.body.password;
+    var salt= crypto.randomBytes(128).toString('hex');
+    var dbString= hash(password, salt);
+    pool.query('INSERT INTO "users" (username, password) VALUES($1, $2)', [username, dbString],
+    function(err, result)
+    {
+        if(err)
+        {
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+            res.send('User successfully created: ' + username);
+        }
+    });
+    
 });
 
 
