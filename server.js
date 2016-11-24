d0ccb4c8c7dc51176e4c04293b12a59279be07ee
AjaSharma93 <aja.sharma1101@gmail.com>
2016-11-24 14:28:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -528,7 +528,7 @@ function articleTemplate(data, commentData)
 			</head>
 			<body>
 				<div class="container-fluid">
-					<button id="back" class="left btn btn-default"><i class=" glyphicon glyphicon-chevron-left"></i>Go back</button>
+					<button id="back" class="padding-top-100px left btn btn-default"><i class=" glyphicon glyphicon-chevron-left"></i>Go back</button>
 					<h1 class="center">${articleHeading}</h1>
 					<hr/>
 					<img src="${imgsrc}" alt="${articleHeading}"  class="img-responsive center-block" width="60%"/>
