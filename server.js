f363b1ecfdd638ff6b0208747d0bf34aec79269f
AjaSharma93 <aja.sharma1101@gmail.com>
2016-11-25 17:09:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -378,6 +378,11 @@ function createTemplate(data){
 					  });
 				  });
 			</script>
+			<script>
+              (function (w,i,d,g,e,t,s) {w[d] = w[d]||[];t= i.createElement(g);
+                t.async=1;t.src=e;s=i.getElementsByTagName(g)[0];s.parentNode.insertBefore(t, s);
+              })(window, document, '_gscq','script','//widgets.getsitecontrol.com/61950/script.js');
+            </script>
 			</body>
 	</html>	`;
 	return htmlTemplate;
